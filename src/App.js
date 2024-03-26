03401a3d4d7aafab371dc550721e9999ab79eb1f
Dimpo1 <111021090+Dimpo1@users.noreply.github.com>
2024-03-26 14:05:19 +0200

    App html and css update

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is where my weather application will be. 😊
+          <br />
+          Stay tuned! ⏳⏳⏳
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
