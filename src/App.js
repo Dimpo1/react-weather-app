ae3a9db244034dfec044457740cd0665d94d927a
Dimpo1 <111021090+Dimpo1@users.noreply.github.com>
2024-03-26 17:37:38 +0200

    Adding bootstrap and updating the styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Appfooter from "./Appfooter";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App Container">
       <Header />
       <Main />
       <Appfooter />
