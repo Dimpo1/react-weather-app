d9f700bbfb2c60c30c56238da63cf80fcef4bc5c
Dimpo1 <111021090+Dimpo1@users.noreply.github.com>
2024-03-29 15:06:33 +0200

    added API integratin

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Header() {
+  let [ready, setReady] = useState(false);
+  let [temperature, setTemperature] = useState(null);
+  function handleresponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+  }
+  let apikey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
+  let city = "Paris";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+  axios.get(url).then(handleresponse);
   return (
     <header>
       <form id="searchForm">
@@ -10,6 +21,7 @@ export default function Header() {
           id="citySearch"
           placeholder="Enter a city..."
           className="searchInput"
+          autoFocus="on"
           required
         />
         <input type="submit" value="Search" className="search" />
--- src/WeatherData.js
+++ src/WeatherData.js
@@ -1,31 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Header from "./Header";
 
 export default function WeatherData() {
-  return (
-    <div className="WeatherData">
-      <div>
-        <div id="city"></div>
-        <p>
-          <span id="dateTime"> </span> |<span id="description"> </span>
-          <br />
-          humidity:{" "}
-          <strong>
-            <span id="humidity"></span>%
-          </strong>{" "}
-          | Wind:
-          <strong>
-            <span id="windSpeed"></span> km/h
-          </strong>
-        </p>
-      </div>
-      <div id="currentTemperature">
-        <div id="icon"></div>
+  let [ready, setReady] = useState(false);
+  let [temperatureData, setTemperatureData] = useState({});
+  function handleresponse(response) {
+    console.log(response.data.weather[0].description);
+    setTemperatureData({
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      icon: response.data.weather.icon,
+    });
+    setReady(true);
+  }
 
-        <div className="temp" id="temp">
-          <strong></strong>
+  if (ready) {
+    return (
+      <div className="container">
+        <div className="WeatherData">
+          <div className="row">
+            <div className="col-8">
+              <div id="city">{temperatureData.city}</div>
+              <p>
+                <span id="dateTime"> </span> |
+                <span className="text-capitalize" id="description">
+                  {" "}
+                  {temperatureData.description}{" "}
+                </span>
+                <br />
+                Humidity:{" "}
+                <strong>
+                  <span id="humidity">{temperatureData.humidity}</span>%
+                </strong>{" "}
+                | Wind:{" "}
+                <strong>
+                  <span id="windSpeed">{temperatureData.wind}</span> km/h
+                </strong>
+              </p>
+            </div>
+
+            <div className="col-4">
+              <div id="currentTemperature">
+                <div id="icon">
+                  <img
+                    src={temperatureData.icon}
+                    alt={temperatureData.description}
+                  />
+                </div>
+
+                <div className="temp" id="temp">
+                  <strong>{Math.round(temperatureData.temperature)}</strong>
+                </div>
+                <div className="unit">°C</div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="unit">°C</div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apikey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
+    let city = "Paris";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+    axios.get(url).then(handleresponse);
+
+    return "Loading...";
+  }
 }
